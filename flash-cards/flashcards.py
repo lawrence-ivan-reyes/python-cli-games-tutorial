11254be3d16900fb4bdbb8574a9eb02ca61e2e40
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-09-11 19:10:36 -0400

    completed flash card tester

---
+++ flash-cards/flashcards.py
@@ -0,0 +1,34 @@
+# Importing the JSON Module from Python3
+import json
+
+# Opening the File and Parsingh the JSON
+with open('me-capitals.json', 'r') as f:
+    data = json.load(f)
+    print(data)
+
+# Initializing "Total" as the Length of the "cards" Array & Setting the Base Score as 0
+total = len(data["cards"])
+score = 0
+
+for i in data["cards"]:
+    guess = input(i["q"] + ">")
+    
+    if guess == i["a"]:
+        score +=1
+        print(f"Correct! Current Score: {score}/{total}")
+    else:
+        print("Incorrect! The correct answer was", i["a"])
+        print(f"Current Score: {score}/{total}")
+
+# Adding an End-Game Message
+print(f"Thank you for playing! You scored a total of {score} points out of {total}.")
+
+# Adding to the End-Game Message based on the User's Score
+if score == total:
+    print("Amazing! You're a capital wizard.")
+
+if score > total / 2:
+    print("Good job! You passed with over 50%.")
+
+else:
+    print("Grab yourself a map and start studying your capitals! Good luck next time.")
