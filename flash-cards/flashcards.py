fcc2496bb9c7496de09f14b7c58b498787a7588b
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-09-15 15:32:22 -0400

    revised to remove the printed answers to the output

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,20 +1,15 @@
-# Starting Message
 print("Welcome to 'Guess the Capital', where we'll be testing your Middle Eastern country knowledge. Let's begin!\n")
 
-# Importing the JSON Module from Python3
 import json
 
-# Opening the File and Parsingh the JSON
 with open('me-capitals.json', 'r') as f:
     data = json.load(f)
-    print(data)
 
-# Initializing "Total" as the Length of the "cards" Array & Setting the Base Score as 0
 total = len(data["cards"])
 score = 0
 
 for i in data["cards"]:
-    guess = input(i["q"] + ">")
+    guess = input(i["q"] + "> ")
     
     if guess == i["a"]:
         score +=1
@@ -23,10 +18,8 @@ for i in data["cards"]:
         print("Incorrect! The correct answer was", i["a"])
         print(f"Current Score: {score}/{total}")
 
-# Adding an End-Game Message
 print(f"Thank you for playing! You scored a total of {score} points out of {total}.\n")
 
-# Adding to the End-Game Message based on the User's Score
 if score == total:
     print("Amazing! You're a capital wizard.")
 
